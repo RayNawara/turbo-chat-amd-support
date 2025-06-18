001e00dae048bc56e476c76bf1f5a924bd85257f
RayNawara <rjnawara@gmail.com>
2025-06-18 13:35:58 -0400

    Fixed private docker registry by exporting registry password; added new small model for running without GPU

--- app/models/ai_chat.rb
+++ app/models/ai_chat.rb
@@ -6,7 +6,7 @@ class AiChat < ApplicationRecord
   has_many :ai_messages, -> { order(id: :asc) }, dependent: :delete_all
 
   SUPPORTED_AI_MODELS = {
-    text: %w[mistral:7b-instruct-v0.3-q8_0 mixtral:8x7b-instruct-v0.1-q5_K_M codellama:7b-instruct codellama:34b-instruct phind-codellama:34b-v2 starcoder2:15b
+    text: %w[phi3:mini mistral:7b-instruct-v0.3-q8_0 mixtral:8x7b-instruct-v0.1-q5_K_M codellama:7b-instruct codellama:34b-instruct phind-codellama:34b-v2 starcoder2:15b
  dolphin-llama3:8b llama3.1:latest jean-luc/big-tiger-gemma:27b-v1c-Q3_K_M vanilj/mistral-nemo-12b-celeste-v1.9:Q4_K_M qwen2.5-coder gemma2],
     image: %w[anythingV3_fp16.safetensors realisticVisionV60B1_v51HyperVAE.safetensors sdxl-turbo sdxl-anime ponyDiffusionV6XL_v6StartWithThisOne]
   }.freeze
